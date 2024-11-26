092b24dae4d62039acdf4b6e657710555a8b2fbd
kadir <a.kadirgrgn@gmail.com>
2024-11-26 23:03:53 +0300

    Uygulama test yazimi yapildi

---
+++ KitapKosesiTests/KitapKosesiTests.swift
@@ -0,0 +1,16 @@
+//
+//  KitapKosesiTests.swift
+//  KitapKosesiTests
+//
+//  Created by Kadir on 26.11.2024.
+//
+
+import Testing
+
+struct KitapKosesiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
